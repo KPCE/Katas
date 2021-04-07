8bb2d5b41633ea05f7ce102018fa26fddb8b5532
Jaime <jamesreuss1@gmail.com>
2021-04-07 14:28:49 -0700

    repeating-numbers done

--- repeating-numbers.js
+++ repeating-numbers.js
@@ -1,13 +1,17 @@
 const repeatNumbers = function(data) {
+  let num = ''
   for (let i = 0; i < data.length; i++){
-    let num = ''
-    for (let j = 0; j = data[i][1]; j++){
+    for (let j = 0; j < data[i][1]; j++){
       num += data[i][0]
+      if (data[i][1] - 1 === j && data.length > 1 && i !== data.length - 1) {
+        num += ', ';
+      }
     }
-    console.log(num)
   }
+  return num;
 };
-
-console.log(repeatNumbers([[1, 10]]));
-console.log(repeatNumbers([[1, 2], [2, 3]]));
-console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+//input - first number is the value to repeat, second is the number of times to repeat it
+//below are test cases
+console.log('1', repeatNumbers([[1, 10]]));
+console.log('2', repeatNumbers([[1, 2], [2, 3]]));
+console.log('3', repeatNumbers([[10, 4], [34, 6], [92, 2]]));
