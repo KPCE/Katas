e237f91d5de6df370f8c5055e0a4a58cc596d0cc
Jaime <jamesreuss1@gmail.com>
2021-04-27 17:37:05 -0700

    completed case-maker

--- case-maker.js
+++ case-maker.js
@@ -1,5 +1,11 @@
 const camelCase = function(input) {
+  const output = input.split(' ');
   
+  for (let i = 1; i <= output.length - 1; i++) {
+    output[i] = output[i][0].toUpperCase() + output[i].substr(1);
+  }
+  
+  return output.join('');
 };
 
 console.log(camelCase("this is a string"));
